7282eb9d86d807edae27694ffe3d90baeb70fb21
yyeonjju <llilyha123@gmail.com>
2024-05-26 13:17:12 +0900

    Initial Commit

---
+++ TravelPlatform/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelPlatform
+//
+//  Created by 하연주 on 5/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
